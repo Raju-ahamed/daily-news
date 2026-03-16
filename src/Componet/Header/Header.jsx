417451bb16b7a829f123f4fdfde75c255a59f947
Raju-ahamed <rajuahamed7320@gmail.com>
2026-03-17 00:22:01 +0600

    files

--- src/Componet/Header/Header.jsx
+++ src/Componet/Header/Header.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import logo from "../../assets/logo.png"
+import { format } from 'date-fns';
 
 const Header = () => {
     return (
-        <div className='text-center'>
+        <div className='text-center mt-4 text-accent'>
             <img className=' mx-auto w-100 m-2' src={logo} alt="" />
             <p>Journalism Without Fear or Favour</p>
-            <p></p>
+            <p><span className='text-primary'>{format(new Date(), "EEEE")} </span>{format(new Date(), "MMMM dd, uuuu")} </p>
         </div>
     );
 };
---
+++ src/Componet/LatestNews/LeatestNews.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Marquee from 'react-fast-marquee';
+
+const LeatestNews = () => {
+    return (
+        <div className='flex items-center gap-2 w-11/12 mx-auto'>
+            <h1 className='btn btn-secondary mx-3 my-2 p-2'>Latest News</h1>
+            <Marquee className='font-bold' speed={60} pauseOnHover={true} gradient={false}>  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore, corrupti?</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore, corrupti?</p>
+            </Marquee>
+        </div>
+    );
+};
+
+export default LeatestNews;
